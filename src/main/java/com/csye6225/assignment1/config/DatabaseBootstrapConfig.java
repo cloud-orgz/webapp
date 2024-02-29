4976a439d07821887da4fb5cbad20ce8b91cb43e
MukulSaiPendem <144606414+MukulSaiPendem@users.noreply.github.com>
2024-02-29 10:28:48 -0500

    Update DatabaseBootstrapConfig.java

--- src/main/java/com/csye6225/assignment1/config/DatabaseBootstrapConfig.java
+++ src/main/java/com/csye6225/assignment1/config/DatabaseBootstrapConfig.java
@@ -13,10 +13,14 @@ public class DatabaseBootstrapConfig {
     @Autowired
     private DataSource dataSource;
 
+    @Value("${DB_NAME}") // This line injects the DB_NAME value from your .env file
+    private String dbName;
+
     @PostConstruct
     public void createDatabase() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        jdbcTemplate.execute("CREATE DATABASE IF NOT EXISTS cloud");
+        String sql = String.format("CREATE DATABASE IF NOT EXISTS %s", dbName); // Use the dbName variable
+        jdbcTemplate.execute(sql);
     }
 }
 
