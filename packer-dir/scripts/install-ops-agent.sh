da751a5e328e58fcfed8eaf1cdd16c2cd8685093
Mukul sai Pendem <pendem.mu@northeastern.edu>
2024-03-18 16:30:25 -0400

    ops-agent

---
+++ packer-dir/scripts/install-ops-agent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Download and install the Google Cloud Ops Agent
+echo "Downloading and installing Google Cloud Ops Agent..."
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+sudo mv /tmp/ops-agent-config.yaml /etc/google-cloud-ops-agent/config.yaml
+
+sudo systemctl restart google-cloud-ops-agent
+
+echo "Google Cloud Ops Agent installation and configuration complete."
