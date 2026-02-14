7da2e5757c5d276e0dbad852025eefa13168de70
MukulSaiPendem <pendem.mu@northeastern.edu>
2026-02-14 10:24:05 -0600

    update code for vertex ai to give risk score based on user created

--- src/main/java/com/csye6225/assignment1/Service/Impl/PubSubPublisherServiceImpl.java
+++ src/main/java/com/csye6225/assignment1/Service/Impl/PubSubPublisherServiceImpl.java
@@ -1,6 +1,7 @@
 package com.csye6225.assignment1.Service.Impl;
 
 import com.csye6225.assignment1.Service.PubSubPublisherService;
+import com.csye6225.assignment1.Service.RiskScoringService;
 import com.csye6225.assignment1.entities.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -9,13 +10,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PubSubPublisherServiceImpl implements PubSubPublisherService {
 
     @Autowired
     private PubSubTemplate pubSubTemplate;
+
+    @Autowired
+    private RiskScoringService riskScoringService;
 
     private final ObjectMapper objectMapper;
 
@@ -26,7 +33,6 @@ public class PubSubPublisherServiceImpl implements PubSubPublisherService {
 
     public PubSubPublisherServiceImpl() {
         this.objectMapper = new ObjectMapper();
-        // Register the JavaTimeModule to handle Java 8 Date and Time API types
         this.objectMapper.registerModule(new JavaTimeModule());
     }
 
