9068310c1e3ca644373e5587bc6f8ef26e78ec8a
MukulSaiPendem <pendem.mu@northeastern.edu>
2026-02-14 13:52:12 -0600

    refactor webapp

--- src/main/java/com/identityhub/webapp/Service/Impl/CustomeUserDetailsService.java
+++ src/main/java/com/identityhub/webapp/Service/Impl/CustomeUserDetailsService.java
@@ -1,9 +1,7 @@
 package com.identityhub.webapp.Service.Impl;
 
-import com.identityhub.webapp.entities.User;
 import com.identityhub.webapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,16 +20,14 @@ public class CustomeUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println(username);
-        User user = userRepository.findByUsername(username)
+        com.identityhub.webapp.entities.User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        // Here you should build the UserDetails object however you need to.
-        // For example, you might construct a Spring Security User like this:
         System.out.println(user.getPassword());
-        return User.builder()
+        return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getUsername())
-                .password(user.getPassword()) // Make sure this password is encoded using the same PasswordEncoder bean
-                .authorities("USER") // Assign authorities or roles as needed
+                .password(user.getPassword())
+                .authorities("USER")
                 .build();
     }
 }
